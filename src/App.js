f6d95ecfa591da68f231441be3fdd23875025bc0
Tupini <piradinho1@gmail.com>
2019-01-29 01:42:05 -0200

    Add API integration

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import { Jumbotron } from 'reactstrap'
 
 //Components
 import Form from './Form'
 import List from './List'
+
+const API_URL = 'http://localhost:3030/todo/'
 
 class App extends Component {
 		constructor(props){
@@ -14,15 +17,41 @@ class App extends Component {
 			this.addNewTodo = this.addNewTodo.bind(this)
 			this.destroyTodo = this.destroyTodo.bind(this)
 		}
-	
+		
+		componentDidMount(){
+			axios.get(API_URL)
+				.then(res => {
+					let items = res.data
+					this.setState({items})
+				})
+				.catch(e => console.error(e))
+		}
+		
+		fetchTodos(){
+			axios.get(API_URL)
+			.then(res => {
+				let items = res.data
+				this.setState({items})
+			})
+			.catch(e => console.error(e))
+		}
+
 		addNewTodo(todo){
-			let items = [...this.state.items,{...todo,id:this.state.items.length}]
-			this.setState({items})
+			axios.post(API_URL,todo)
+				.then(res => {
+					this.fetchTodos()
+				})
+				.catch(e => console.error(e))
 		}
 
 		destroyTodo(id){
-			let items = this.state.items.filter(item => (item.id != id) && item)
-			this.setState({items})
+			axios.delete(API_URL+id)
+				.then(res => {
+					this.fetchTodos()
+				})
+				.catch(e => console.error(e))
+//			let items = this.state.items.filter(item => (item.id !== id) && item)
+//			this.setState({items})
 		}
 
   render() {
